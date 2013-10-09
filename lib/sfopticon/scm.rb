bf68fdaa36d1dd46dd0d3731cff3ff5e5605cb30
Ryan Parr <ryanwparr@gmail.com>
2013-10-08 17:01:12 -0700

    Added a static method to retrieve the class of the SCM adapter as a constant

--- lib/sfopticon/scm.rb
+++ lib/sfopticon/scm.rb
@@ -1,27 +1,16 @@
 # This class provides a sort of factory for the actual SCM adapter
 #
-# SCM adapters should accept named arguments in the constructor
-# which add to and override the configuration under the SCM options
-# in the configuration YAML.
-#
-# SCM adapters should all define the following methods:
-# repo_exists?::
-# create_repo:: This will create a new remote repository
-# delete_repo:: Deletes the remote repository
-# create_branch(name):: Forks from HEAD
-# delete_branch(name):: Deletes named branch
-# add:: Recursive addition of all changes to the working copy
-# delete(listOfFiles[]):: Deletion of specific files from the working copy
-# commit:: Commits, and pushes to remote if necessary
-#
-
+# (see #SfOpticon::Scm::Base)
 class SfOpticon::Scm
-	def self.new(*args)
+	def self.adapter
 		adapter_lib = File.dirname(__FILE__) + '/scm/'  \
 		            + SfOpticon::Settings.scm.adapter \
 		            + '.rb'
 		load adapter_lib
-		klass = "SfOpticon::Scm::#{SfOpticon::Settings.scm.adapter.capitalize}".constantize
-		klass.new(*args)
+		"SfOpticon::Scm::#{SfOpticon::Settings.scm.adapter.capitalize}".constantize		
+	end
+
+	def self.new(*args)
+		adapter.new(*args)
 	end
 end
