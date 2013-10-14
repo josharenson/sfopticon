afd495ef6a7800c538ef96cd3f79d8315f60cea8
Ryan Parr <ryanwparr@gmail.com>
2013-10-14 09:15:06 -0700

    #32 - Restoring establish_connection

--- lib/sfopticon/db/schema.rb
+++ lib/sfopticon/db/schema.rb
@@ -1,2 +1,6 @@
 require 'sfopticon/db/models/environments'
 require 'sfopticon/db/models/sf_objects'
+
+class SfOpticon::Schema
+	ActiveRecord::Base.establish_connection(SfOpticon::Settings.database)
+end
