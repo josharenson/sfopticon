8d92d4d64afd718d08d9a07049f84036deaf01c3
Ryan Parr <ryanwparr@gmail.com>
2013-10-13 20:58:37 -0700

    #16 - Migrations now working. Schema declarations have been moved to lib/sfopticon/db/migrations.

---
+++ lib/sfopticon/db/migrations/20131013204844_create_environments.rb
@@ -0,0 +1,10 @@
+class CreateEnvironments < ActiveRecord::Migration
+  def change
+  	create_table :environments, :force => true do |t|
+		t.string   "name", :unique => true, :null => false
+		t.string   "username"
+		t.string   "password"
+		t.boolean  "production"
+	end
+  end
+end
